aec9b293de10133b7e25d7a4f1fc051ba5459528
David Schneider <schneider@puzzle.ch>
2022-03-25 16:45:10 +0100

    Create CLI with clap

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,56 @@
-use rand::Rng;
+use clap::{Parser, Subcommand};
 
 mod user;
+
+#[derive(Parser)]
+struct RootCommand {
+    #[clap(short, long, default_value = "sqlite://data.db")]
+    connect_string: String,
+
+    #[clap(subcommand)]
+    command: Command
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Get {
+        id: Option<uuid::Uuid>,
+    },
+    Create {
+        name: String,
+        age: u8,
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //  for SQLite, use SqlitePoolOptions::new()
+    let cmd = RootCommand::parse();
+
 
     let pool = sqlx::sqlite::SqlitePoolOptions::new()
-        .connect("sqlite://data.db")
+        .connect(cmd.connect_string.as_str())
         .await?;
-
-    let mut rng = rand::thread_rng();
-
-    let user = user::User::new(String::from("rand person1"), rng.gen());
 
     let store = user::SQLiteUserStore::new(pool);
 
-    store.create_user(user).await?;
-
-    let users = store.get_users().await?;
-
-    for user in users {
-        println!("{user:?}");
+    match cmd.command {
+        Command::Create { name, age } => {
+            let user = user::User::new(name, age);
+            store.create_user(user).await?;
+        }
+        Command::Get { id } => match id {
+            Some(id) => {
+                let user = store.get_user_by_id(id).await?;
+                println!("{user:?}");
+            }
+            None => {
+                let users = store.get_users().await?;
+                for user in users {
+                    println!("{user:?}")
+                }
+            }
+        },
     }
     Ok(())
 }
